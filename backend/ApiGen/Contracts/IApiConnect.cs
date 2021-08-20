38ec2e1eca224db9f798b9112284eafb588ce90d
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 10:57:49 +0700

    Update IApiConnect.cs

--- backend/ApiGen/Contracts/IApiConnect.cs
+++ backend/ApiGen/Contracts/IApiConnect.cs
@@ -1,10 +1,1 @@
-﻿using System.Threading.Tasks;
 
-namespace ApiGen.Contracts
-{
-    public interface IApiConnect
-    {
-        Task<TResponse> PostDataAsync<TResponse, TRequest>(string endPoint, TRequest request);
-        Task<TResponse> GetDataAsync<TResponse>(string endPoint)
-    }
-}
