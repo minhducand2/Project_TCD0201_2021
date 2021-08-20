2b06ca2bf1836804901e7f991e9d8f71219559de
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 09:11:13 +0700

    Update C1400BlogController.cs

--- backend/ApiGen/API/v1/C1400BlogController.cs
+++ backend/ApiGen/API/v1/C1400BlogController.cs
@@ -121,7 +121,7 @@ namespace ApiGen.API.v1
             if (what == 1407)
             { 
                 // Call TOP Blog all data to User table
-                var result = await _d1400BlogDataAccess.CustomJoinBlog();
+                var result ();
 
                 return JsonConvert.SerializeObject(result, Formatting.Indented);
             }
