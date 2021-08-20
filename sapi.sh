43dda31fc1b6aab40488d47df4dc11bc5395e30a
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 10:12:47 +0700

    Update sapi.sh

--- sapi.sh
+++ sapi.sh
@@ -1,2 +1,1 @@
-docker-compose -f docker-compose-api.yml down; docker-compose -f docker-compose-api.yml up --build;
-docker-compose -f docker-compose-api.yml down & docker-compose -f docker-compose-api.yml up --build;
+
