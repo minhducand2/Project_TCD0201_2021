da3c118af9e18a06dad407ff2e45c5f6fd6e9764
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 09:29:01 +0700

    Update C1600ContactStatusController.cs

--- backend/ApiGen/API/v1/C1600ContactStatusController.cs
+++ backend/ApiGen/API/v1/C1600ContactStatusController.cs
@@ -3,9 +3,7 @@ using ApiGen.Data.DataAccess;
 using ApiGen.Data.Entity; 
 using AutoMapper; 
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
 
 namespace ApiGen.API.v1
 {
