303bf1bc4faeae995b209df7242a9523a37ae85a
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 09:12:48 +0700

    Update C1400BlogController.cs

--- backend/ApiGen/API/v1/C1400BlogController.cs
+++ backend/ApiGen/API/v1/C1400BlogController.cs
@@ -4,6 +4,7 @@ using ApiGen.Data.Entity;
 using ApiGen.Infrastructure.Extensions;
 using AutoMapper; 
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
