7cfdcc4a8cf577f84ac75feb08fed7f6fb53e243
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2021-08-20 10:16:06 +0700

    Update sdatabase.sh

--- sdatabase.sh
+++ sdatabase.sh
@@ -1,2 +1,2 @@
 docker-compose -f docker-compose-database.yml down; docker-compose -f docker-compose-database.yml up --build;
-docker-compose -f docker-compose-database.yml down & docker-compose -f docker-compose-database.yml up --build;
+docker-compose -f docker-compose-database.yml down & docker-compose -f docker-compose-database.yml up --build
